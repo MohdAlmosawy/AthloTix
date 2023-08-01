3ec61626002d7d4e97feccbe9a1c7d773128906d
mq1977 <qarooni197@gmail.com>
2023-08-01 16:44:55 +0300

    t2

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const usersCtrl = require('../controllers/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+
+router.get('/new',usersCtrl.new);
+router.get('/new',usersCtrl.create);
 
 module.exports = router;
+
